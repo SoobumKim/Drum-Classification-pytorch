59ac832188a5dfaa4410dc8c3165894473e09946
SoobumKim <subum42@naver.com>
2021-06-15 12:44:49 +0900

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .vgg19 import VGG19
 from .resnet34 import ResNet34, ResidualBlock
 from .efficientnet import EfficientNet, VALID_MODELS
+
 from .utils import (
     GlobalParams,
     BlockArgs,
